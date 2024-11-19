9c1c75b46e9097325285e67c21bbaf824cb4d307
FRex <blazej.roszkowski@gmail.com>
2024-11-19 02:26:23 +0100

    added shortcuts

--- DataTable.h
+++ DataTable.h
@@ -1,17 +1,25 @@
 #pragma once
 
+/* DataTable is opaque type the definition is in the .c file */
 typedef struct DataTable DataTable;
 
+/* create, destroy, resize and set default element size */
 DataTable * DataTable_create(void);
 void DataTable_destroy(DataTable * d);
-void DataTable_setDefaultElementSize(DataTable * d, int size);
 void DataTable_resize(DataTable * d, int newbucketamount);
+void DataTable_setDefaultElementSize(DataTable * d, int size);
 
+/* high level API, shortcuts for DataTable_operation */
+#define DataTable_find(d, key) DataTable_operation(d, EDTO_FIND, key, -1, -1, NULL)
+#define DataTable_findOrAdd(d, key) DataTable_operation(d, EDTO_FIND_OR_ADD, key, -1, -1, NULL)
+#define DataTable_remove(d, key) DataTable_operation(d, EDTO_REMOVE, key, -1, -1, NULL)
+
+/* the low level API, all other apis are shortcuts using this one */
 typedef enum EDATA_TABLE_OPERATION {
     EDTO_FIND,
     EDTO_FIND_OR_ADD,
     EDTO_REMOVE,
-    // TODO: EDTO_REPLACE
+    /* TODO: EDTO_REPLACE? */
 } EDATA_TABLE_OPERATION;
 
 void * DataTable_operation(DataTable * d, EDATA_TABLE_OPERATION op, const char * key, int keylen, int datasize, int * outdatasize);
--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(int argc, char ** argv)
 
     for(int i = 1; i < argc; ++i)
     {
-        int * x = (int*)DataTable_operation(d, EDTO_FIND_OR_ADD, argv[i], 0, 0, NULL);
+        int * x = (int*)DataTable_findOrAdd(d, argv[i]);
         *x = i;
     }
 
@@ -16,7 +16,7 @@ int main(int argc, char ** argv)
 
     for(int i = 1; i < argc; ++i)
     {
-        const int * x = (const int*)DataTable_operation(d, EDTO_FIND, argv[i], 0, 0, NULL);
+        const int * x = (const int*)DataTable_find(d, argv[i]);
         printf("%d - %s\n", i, (*x == i) ? "ok" : "ERROR!!");
     }
 
